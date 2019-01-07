cfc59b417f00aeffcee546f46269de23c9adf313
Tuan Luong <tuanluong.it@gmail.com>
2019-01-07 17:09:51 +0700

    update ios

--- ios/RNEasyPref.m
+++ ios/RNEasyPref.m
@@ -9,5 +9,20 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_EXPORT_METHOD(set:(NSString*)key value:(id)value
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+{
+    [[NSUserDefaults standardUserDefaults] setObject:value forKey:key];
+}
+
+RCT_EXPORT_METHOD(get:(NSString*)key
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+{
+    id value = [[NSUserDefaults standardUserDefaults] objectForKey:key];
+    resolve(value);
+}
+
 @end
   
