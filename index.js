a94c044893385eded39a96dc58f80971d34fe719
Tuan Luong <tuanluong.it@gmail.com>
2019-01-07 16:42:59 +0700

    update api

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const { RNEasyPref } = NativeModules;
 
-export default RNEasyPref;
+const getBoolean = async (key: string, defaultVal: any) => {
+    if (Platform.OS === 'ios') {
+        const value = await RNEasyPref.get(key);
+        if (value != undefined) {
+            return value;
+        }
+        return defaultVal;
+    } else {
+        return RNEasyPref.getBoolean(key, defaultVal);
+    }
+};
+
+const set = async (key, value) => {
+	RNEasyPref.set(key, value);
+};
+
+export default {
+    getBoolean,
+    set,
+};
