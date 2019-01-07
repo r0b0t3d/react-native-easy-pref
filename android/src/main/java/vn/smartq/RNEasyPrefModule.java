c32f1133775f804bccbf46ed15445255b73db28d
Tuan Luong <tuanluong.it@gmail.com>
2019-01-07 16:15:17 +0700

    update android

--- android/src/main/java/vn/smartq/RNEasyPrefModule.java
+++ android/src/main/java/vn/smartq/RNEasyPrefModule.java
@@ -1,6 +1,8 @@
 
 package vn.smartq;
 
+import android.widget.Toast;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -8,15 +10,20 @@ import com.facebook.react.bridge.Callback;
 
 public class RNEasyPrefModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNEasyPrefModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNEasyPrefModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNEasyPref";
-  }
+    @Override
+    public String getName() {
+        return "RNEasyPref";
+    }
+
+    @ReactMethod
+    public void show(String message) {
+        Toast.makeText(getReactApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
 }
