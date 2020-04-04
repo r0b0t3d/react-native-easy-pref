c332b906b685ea731c8c4da04144b118ba0ea441
Tuan Luong <tuanluong.it@gmail.com>
2020-04-04 18:28:53 +0700

    remove lib folder

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: {
-    getBoolean: (key: string, defaultVal: any) => Promise<any>;
-    getString: (key: string, defaultVal: string) => Promise<any>;
-    getInt: (key: string, defaultVal: string) => Promise<any>;
-    setInt: (key: string, value: number) => Promise<void>;
-    setString: (key: string, value: string) => Promise<void>;
-    setBoolean: (key: string, value: string) => Promise<void>;
-};
-export default _default;
